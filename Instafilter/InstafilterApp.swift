86b9ec3d2d6d4ec3987baea8d181288f3e77112e
BizMagnets <bizmagnets@BizMagnetss-Mac-mini.local>
2025-07-04 09:22:31 +0530

    inital commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by BizMagnets on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
